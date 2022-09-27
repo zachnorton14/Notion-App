0d8eca1193f388d1e6ec196510b3e135dc88602b
okayzach <znortyy@gmail.com>
2022-09-26 23:31:07 -0400

    created basic file structure and frontend layout

--- notion-app/src/App.js
+++ notion-app/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Error404 from './Error404';
+import Dashboard from './components/Dashboard';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Error404 />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ notion-app/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+
+
+function Dashboard() {
+
+    return (
+        <h1>dashboard</h1>
+    )
+}
+
+export default Dashboard
